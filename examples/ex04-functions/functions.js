09936bfe6bc4450abd22fdf95c1749634ae1c777
Mattia A. Fritz <mattia.fritz@unige.ch>
2016-10-26 11:58:48 +0200

    Added callback functions

--- examples/ex04-functions/functions.js
+++ examples/ex04-functions/functions.js
@@ -38,3 +38,37 @@ function double(num) {
 
 var twenty = double(10);
 console.log(twenty); //--> 20;
+
+/* MORE DIFFICULT */
+
+/* Since a function accepts any kind of argument, you can also pass a function inside another function. This usually refers to a callback function */
+//The callback function
+function myCallBackFunction(textToLog) {
+    var message = textToLog.toUpperCase();
+    console.log(message);
+}
+
+//The main function
+function myMainFunction(aNormalArgument, theFunction) {
+    var greeting = "Hello my dear " + aNormalArgument;
+    //Now we call the function as argument (callback)
+    theFunction(greeting);
+}
+
+//Invocation of the two functions
+myMainFunction("user", myCallBackFunction); //--> HELLO MY DEAR USER
+
+//We may define another callback function
+function anotherCallBackFunction(anotherArgument) {
+    var reverse = anotherArgument.split("").reverse().join("");
+    console.log(reverse);
+}
+
+//Invocation of the main function with another callback
+myMainFunction("another user", anotherCallBackFunction); //--> resu rehtona raed ym olleH
+
+//Finally, it is possible to use an anonymous function as a callback. The invocation in this case is immediate //-->Hello my dear guest
+myMainFunction("anonymous", function (message) {
+    var replace = message.replace("anonymous", "guest");
+    console.log(replace);
+});
